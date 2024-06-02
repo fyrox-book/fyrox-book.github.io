3f94aa5e5c8f9322779e827be122b1977b66047a
mrDIMAS <mrDIMAS@users.noreply.github.com>
2024-06-02 12:29:16 +0000

    deploy: 644027fe0a4987951a6571042a6ed73985149f61

---
+++ code/snippets/src/resource/model.rs
@@ -0,0 +1,21 @@
+use fyrox::{
+    asset::manager::ResourceManager,
+    core::pool::Handle,
+    resource::model::{Model, ModelResourceExtension},
+    scene::{node::Node, Scene},
+};
+use std::path::Path;
+
+// ANCHOR: instantiate_model
+async fn instantiate_model(
+    path: &Path,
+    resource_manager: ResourceManager,
+    scene: &mut Scene,
+) -> Handle<Node> {
+    // Load model first. Alternatively, you can store resource handle somewhere and use it for
+    // instantiation.
+    let model = resource_manager.request::<Model>(path).await.unwrap();
+
+    model.instantiate(scene)
+}
+// ANCHOR_END: instantiate_model
