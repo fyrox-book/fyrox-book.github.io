06c989984dd4e4db547c1966c98382a7633fdcfd
Dmitry Stepanov <d1maxa@yandex.ru>
2024-06-02 11:13:34 +0300

    fixed plugin docs

--- src/code/snippets/src/scripting/context.rs
+++ src/code/snippets/src/scripting/context.rs
@@ -1,11 +1,17 @@
 use fyrox::asset::manager::ResourceManager;
 use fyrox::core::pool::Handle;
 use fyrox::engine::task::TaskPoolHandler;
-use fyrox::engine::{GraphicsContext, ScriptMessageDispatcher};
+use fyrox::engine::{
+    AsyncSceneLoader, GraphicsContext, PerformanceStatistics, ScriptMessageDispatcher,
+    ScriptProcessor, SerializationContext,
+};
+use fyrox::event_loop::EventLoopWindowTarget;
+use fyrox::gui::constructor::WidgetConstructorContainer;
 use fyrox::gui::UiContainer;
 use fyrox::scene::node::Node;
-use fyrox::scene::Scene;
+use fyrox::scene::{Scene, SceneContainer};
 use fyrox::script::{PluginsRefMut, ScriptMessageSender};
+use std::sync::Arc;
 
 // ANCHOR: context
 pub struct ScriptContext<'a, 'b, 'c> {
@@ -24,3 +30,22 @@ pub struct ScriptContext<'a, 'b, 'c> {
     pub script_index: usize,
 }
 // ANCHOR_END: context
+
+// ANCHOR: plugin_context
+pub struct PluginContext<'a, 'b> {
+    pub scenes: &'a mut SceneContainer,
+    pub resource_manager: &'a ResourceManager,
+    pub user_interfaces: &'a mut UiContainer,
+    pub graphics_context: &'a mut GraphicsContext,
+    pub dt: f32,
+    pub lag: &'b mut f32,
+    pub serialization_context: &'a Arc<SerializationContext>,
+    pub widget_constructors: &'a Arc<WidgetConstructorContainer>,
+    pub performance_statistics: &'a PerformanceStatistics,
+    pub elapsed_time: f32,
+    pub script_processor: &'a ScriptProcessor,
+    pub async_scene_loader: &'a mut AsyncSceneLoader,
+    pub window_target: Option<&'b EventLoopWindowTarget<()>>,
+    pub task_pool: &'a mut TaskPoolHandler,
+}
+// ANCHOR_END: plugin_context
---
+++ src/code/snippets/src/scripting/plugin.rs
@@ -0,0 +1,109 @@
+use fyrox::gui::inspector::editors::PropertyEditorDefinitionContainer;
+use fyrox::{
+    core::{pool::Handle, reflect::prelude::*, visitor::prelude::*},
+    event::Event,
+    gui::message::UiMessage,
+    plugin::{Plugin, PluginContext, PluginRegistrationContext},
+    scene::Scene,
+};
+use std::path::Path;
+
+// ANCHOR: plugin_structure
+#[derive(Visit, Reflect, Debug)]
+pub struct Game {
+    scene: Handle<Scene>,
+}
+
+impl Game {
+    pub fn new(scene_path: Option<&str>, context: PluginContext) -> Self {
+        context
+            .async_scene_loader
+            .request(scene_path.unwrap_or("data/scene.rgs"));
+
+        Self {
+            scene: Handle::NONE,
+        }
+    }
+}
+
+impl Plugin for Game {
+    fn register(&self, context: PluginRegistrationContext) {
+        // Register scripts here.
+    }
+
+    fn register_property_editors(&self) -> PropertyEditorDefinitionContainer {
+        // Register custom property editors for the editor here.
+        PropertyEditorDefinitionContainer::empty()
+    }
+
+    // ANCHOR: plugin_init
+    fn init(&mut self, scene_path: Option<&str>, context: PluginContext) {
+        // Do initialization logic here. Usually it just requests a scene:
+        context
+            .async_scene_loader
+            .request(scene_path.unwrap_or("data/scene.rgs"));
+    }
+    // ANCHOR_END: plugin_init
+
+    fn on_loaded(&mut self, context: PluginContext) {
+        // For hot reloading only! Only for development.
+        // Re-initialize non-serializable data.
+    }
+
+    fn on_deinit(&mut self, _context: PluginContext) {
+        // Do a cleanup here.
+    }
+
+    fn update(&mut self, _context: &mut PluginContext) {
+        // Add your global update code here.
+    }
+
+    fn on_os_event(&mut self, _event: &Event<()>, _context: PluginContext) {
+        // Do something on OS event here.
+    }
+
+    fn on_graphics_context_initialized(&mut self, context: PluginContext) {
+        // Executed when graphics context was initialized.
+    }
+
+    fn before_rendering(&mut self, context: PluginContext) {
+        // Executed before rendering begins.
+    }
+
+    fn on_graphics_context_destroyed(&mut self, context: PluginContext) {
+        // Executed when graphics context was destroyed.
+    }
+
+    fn on_ui_message(&mut self, _context: &mut PluginContext, _message: &UiMessage) {
+        // Handle UI events here.
+    }
+
+    fn on_scene_begin_loading(&mut self, path: &Path, context: &mut PluginContext) {-
+        context.window_target.unwrap().ex
+        // Handle started scene loading here.
+    }
+
+    fn on_scene_loaded(
+        &mut self,
+        _path: &Path,
+        scene: Handle<Scene>,
+        data: &[u8],
+        context: &mut PluginContext,
+    ) {
+        if self.scene.is_some() {
+            context.scenes.remove(self.scene);
+        }
+
+        self.scene = scene;
+    }
+
+    fn on_scene_loading_failed(
+        &mut self,
+        path: &Path,
+        error: &VisitError,
+        context: &mut PluginContext,
+    ) {
+        // Handle failed scenes here.
+    }
+}
+// ANCHOR_END: plugin_structure
