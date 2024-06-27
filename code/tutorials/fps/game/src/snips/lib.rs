e63e00f9d09774bb7af710d5b6a44ac64222d96c
mrDIMAS <mrDIMAS@users.noreply.github.com>
2024-06-27 17:17:30 +0000

    deploy: 877e46f804783efb92a3802d9af0e16fa3cd7cef

---
+++ code/tutorials/fps/game/src/snips/lib.rs
@@ -0,0 +1,20 @@
+
+// ANCHOR: player_imports
+
+use crate::{player::Player};
+use fyrox::{
+    core::pool::Handle,
+    core::{reflect::prelude::*, visitor::prelude::*},
+    plugin::{Plugin, PluginContext, PluginRegistrationContext},
+    scene::Scene,
+    event::Event,
+    gui::message::UiMessage,
+};
+
+use std::path::Path;
+// ANCHOR_END: player_imports
+// ANCHOR: player_mod_reg
+// Add this line
+pub mod player;
+
+// ANCHOR_END: player_mod_reg
