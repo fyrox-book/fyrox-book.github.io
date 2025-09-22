da5fd34708b5e71bb6c762c7f6012eb798834f63
mrDIMAS <mrDIMAS@users.noreply.github.com>
2025-09-22 18:21:57 +0000

    deploy: 3ed26cfd15189c673c81b6c6ba08900fe994050b

---
+++ code/snippets/src/ui/docking_manager.rs
@@ -0,0 +1,57 @@
+use fyrox::{
+    graph::SceneGraph,
+    gui::{
+        core::pool::Handle,
+        dock::{
+            config::DockingManagerLayoutDescriptor, DockingManager, DockingManagerBuilder,
+            TileBuilder, TileContent,
+        },
+        widget::WidgetBuilder,
+        window::{WindowBuilder, WindowTitle},
+        BuildContext, UiNode, UserInterface,
+    },
+};
+
+// ANCHOR: create_docking_manager
+fn create_docking_manager(ctx: &mut BuildContext) -> Handle<UiNode> {
+    let top_window = WindowBuilder::new(WidgetBuilder::new())
+        .with_title(WindowTitle::text("Top Window"))
+        .build(ctx);
+    let bottom_window = WindowBuilder::new(WidgetBuilder::new())
+        .with_title(WindowTitle::text("Bottom Window"))
+        .build(ctx);
+    let root_tile = TileBuilder::new(WidgetBuilder::new())
+        .with_content(TileContent::VerticalTiles {
+            splitter: 0.5,
+            tiles: [
+                TileBuilder::new(WidgetBuilder::new())
+                    // Note that you have to put the window into a separate tile, otherwise
+                    // you'll get unexpected results.
+                    .with_content(TileContent::Window(top_window))
+                    .build(ctx),
+                TileBuilder::new(WidgetBuilder::new())
+                    .with_content(TileContent::Window(bottom_window))
+                    .build(ctx),
+            ],
+        })
+        .build(ctx);
+    DockingManagerBuilder::new(
+        WidgetBuilder::new()
+            .with_child(root_tile)
+            .with_width(500.0)
+            .with_height(500.0),
+    )
+    .build(ctx)
+}
+// ANCHOR_END: create_docking_manager
+
+// ANCHOR: save_layout
+fn save_layout(
+    ui: &UserInterface,
+    docking_manager_handle: Handle<UiNode>,
+) -> Option<DockingManagerLayoutDescriptor> {
+    ui.try_get_of_type::<DockingManager>(docking_manager_handle)
+        .as_ref()
+        .map(|docking_manager| docking_manager.layout(ui))
+}
+// ANCHOR_END: save_layout
