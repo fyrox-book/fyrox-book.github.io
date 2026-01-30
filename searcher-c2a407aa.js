09a59f6c28d0eaf80a9fde59095fc0044786cf86
mrDIMAS <mrDIMAS@users.noreply.github.com>
2026-01-30 10:06:40 +0000

    deploy: cbb8c7ddf46ce5f59a9349c1b565924b2f4a208c

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2383a967.js',
+                path_to_root + 'searchindex-bc99c833.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
