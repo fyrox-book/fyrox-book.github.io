a1430c649ca1296f661dd8c60bd7718df5458bcf
mrDIMAS <mrDIMAS@users.noreply.github.com>
2026-03-10 19:20:38 +0000

    deploy: 2738fd9ea58c4494a1ac37de5c27b9b93bb57063

--- code/tutorials/rpg/executor-wasm/src/lib.rs
+++ code/tutorials/rpg/executor-wasm/src/lib.rs
@@ -1,45 +1,25 @@
 //! Executor with your game connected to it as a plugin.
 use fyrox::core::wasm_bindgen::{self, prelude::*};
+use fyrox::dpi::LogicalSize;
 use fyrox::engine::executor::Executor;
+use fyrox::engine::GraphicsContextParams;
+use fyrox::event_loop::EventLoop;
+use fyrox::window::WindowAttributes;
 use rpg::Game;
 
 #[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn error(msg: String);
-
-    type Error;
-
-    #[wasm_bindgen(constructor)]
-    fn new() -> Error;
-
-    #[wasm_bindgen(structural, method, getter)]
-    fn stack(error: &Error) -> String;
-}
-
-fn custom_panic_hook(info: &std::panic::PanicInfo) {
-    let mut msg = info.to_string();
-    msg.push_str("\n\nStack:\n\n");
-    let e = Error::new();
-    let stack = e.stack();
-    msg.push_str(&stack);
-    msg.push_str("\n\n");
-    error(msg);
-}
-
-#[inline]
-pub fn set_panic_hook() {
-    use std::sync::Once;
-    static SET_HOOK: Once = Once::new();
-    SET_HOOK.call_once(|| {
-        std::panic::set_hook(Box::new(custom_panic_hook));
-    });
-}
-
-#[wasm_bindgen]
 pub fn main() {
-    set_panic_hook();
-    let mut executor = Executor::new();
+    let mut executor = Executor::from_params(
+        EventLoop::new().ok(),
+        GraphicsContextParams {
+            window_attributes: WindowAttributes::default()
+                .with_inner_size(LogicalSize::new(800, 600)),
+            vsync: false,
+            msaa_sample_count: None,
+            graphics_server_constructor: Default::default(),
+            named_objects: false,
+        },
+    );
     executor.add_plugin(Game::default());
     executor.run()
 }
